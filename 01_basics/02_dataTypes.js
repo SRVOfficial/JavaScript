ded73018467b876ec7a5001611267a28298128ad
SRV <76100264+SRVOfficial@users.noreply.github.com>
2023-12-10 08:09:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+// console.log(3 + 3)  console.log(33 + 3)  // error
+// console.log(3 + 3);  console.log(33 + 3) // valid
+
+/*
+// valid but not a good practice
+console(5 
+    + 3)
+*/
+
+let name = "Eren"
+let age = 18
+let isLoggedIn = false
+
+
+// dataTypes in JS
+// number => 0 to 2^53
+// bigInt => use in case bigger number like stock market or Trading, etc
+// string => ""
+// boolean => true/false
+// null => standard value
+// undefined => varibale haven't initialized yet
+// symbol => unique
+
+
+// object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
